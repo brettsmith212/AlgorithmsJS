1adfced140a844125bb3960e02490b7241bbbd33
Brett Smith <brettsmith212@gmail.com>
2022-05-25 13:32:04 -0700

    leetcode 88 merge sorted array

---
+++ algorithms/leetCodeMergeSortedArray.js
@@ -0,0 +1,22 @@
+var merge = function (nums1, m, nums2, n) {
+  let s = n - 1;
+  let f = m - 1;
+  let i = m + n - 1;
+
+  while (s >= 0) {
+    let fVal = nums1[f];
+    let sVal = nums2[s];
+
+    if (fVal > sVal) {
+      nums1[i] = fVal;
+      i--;
+      f--;
+    } else {
+      nums1[i] = sVal;
+      i--;
+      s--;
+    }
+  }
+
+  return nums1;
+};
